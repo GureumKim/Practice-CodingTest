1987da396183f0546bd2abd7c8b783aba048a2c0
GureumKim <90162048+GureumKim@users.noreply.github.com>
2023-12-15 01:34:23 +0900

    [Silver V] Title: 올림픽, Time: 136 ms, Memory: 110536 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -1,15 +1,24 @@
 n, k = map(int,input().split())
 data = [tuple(map(int,input().split())) for _ in range(n)]
 data = sorted(data,key=lambda x: (x[1],x[2],x[3]),reverse=True)
-rank = 0
+rank,cnt = 0,0
 for country in data:
-
     if country[1] != data[rank][1]:
-        rank +=1
+        rank +=cnt
+        cnt = 1
     elif country[2] != data[rank][2]:
-        rank+=1
+        rank+=cnt
+        cnt = 1
     elif country[3] != data[rank][3]:
-        rank+=1
+        rank+=cnt
+        cnt = 1
+    else:
+        cnt += 1
+
+    # print(country[0])
+    # print(rank)
+    # print(cnt)
+    # print("===")
 
     if country[0] == k:
         print(rank+1)
