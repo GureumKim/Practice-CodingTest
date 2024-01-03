e62ab1c98be22a2087c1162ae8c2b34eba5aeaf7
GureumKim <90162048+GureumKim@users.noreply.github.com>
2024-01-03 22:26:27 +0900

    [Silver III] Title: N과 M (3), Time: 1924 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -13,3 +13,11 @@ n,m = map(int,input().split())
 choice = [0]*m
 dfs(n,m)
 # print(cnt)
+
+# 2
+# from itertools import product
+# 
+# n, m = map(int, input().split())
+# 
+# 
+# print("\n".join(list(map(" ".join, product([str(i) for i in range(1, n + 1)], repeat = m)))))
