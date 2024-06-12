39593449761eeede389a79e98850bcb3b66e4331
GureumKim <90162048+GureumKim@users.noreply.github.com>
2024-06-12 16:19:54 +0900

    [level 3] Title: 아이템 줍기, Time: 0.31 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
@@ -0,0 +1,44 @@
+def solution(rectangle, characterX, characterY, itemX, itemY):
+    from collections import deque
+    
+    answer = 0
+    '''
+        | 1 | 1 |
+          -   -  
+        | 1 | 1 |
+          _   _
+        
+        위 상황 같이 'ㄷ' 자일 때 dir 사용 시 잘못된 좌표를 체킹하게 됨
+        이 예외를 해결하기 위해 전체 맵을 2배로 늘려준다
+    '''
+    # 1 1 
+    # 1 1
+    field = [[-1] * 101 for _ in range(101)] # 51 * 2 (1 - 50)
+    
+    for x1, y1, x2, y2 in rectangle:
+        for i in range(2*y1, 2*y2+1):
+            for j in range(2*x1, 2*x2+1):
+                if 2*y1 < i < 2*y2 and 2*x1 < j < 2*x2:
+                    field[i][j] = 0
+                else:
+                    if field[i][j] == -1:
+                        field[i][j] = 1
+    
+    q = deque()
+    q.append((characterX*2, characterY*2, 0))
+    visited = [[False] * 101 for _ in range(101)]
+    visited[characterY*2][characterX*2] = True
+    
+    dir = [(0,1), (0,-1), (1,0), (-1,0)]
+    while q:
+        x, y, dist = q.popleft()
+        if x == itemX*2 and y == itemY*2:
+            return dist/2
+        for xi, yj in dir:
+            dx, dy = x + xi, y + yj
+            if 1<=dx<101 and 1<=dy<101 and field[dy][dx] == 1:
+                if visited[dy][dx]: continue
+                visited[dy][dx] = True
+                q.append((dx,dy,dist+1))
+    
+    return 0
