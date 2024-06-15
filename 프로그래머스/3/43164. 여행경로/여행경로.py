7e88daceec33a70989ed309c10a300deaa50968f
GureumKim <90162048+GureumKim@users.noreply.github.com>
2024-06-15 17:12:15 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -10,6 +10,9 @@ def solution(tickets):
     
     stack = ['ICN']
     
+    # 꼭 연결되게 가야 되는 게 아니라
+    # 모든 티켓을 이용해서, 모든 도시를 방문하면 되는 거임
+    # 하나의 네트워크로 경로를 연결시키라는 조건은 없음!!!
     while stack:
         while airports[stack[-1]]:
             stack.append(heapq.heappop(airports[stack[-1]]))
