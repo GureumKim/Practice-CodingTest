500114d044c58949b83cbc58cd107a4aecb342e2
GureumKim <90162048+GureumKim@users.noreply.github.com>
2024-06-17 23:01:57 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 272 ms, Memory: 15388 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -1,9 +1,11 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String args[]) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
 		for (int i=0; i < n; i++) {
 			for (int j = n-1-i; j >0 ; j-- ) {
 				System.out.print(" ");
@@ -13,5 +15,6 @@ public class Main {
 			}
 			System.out.print("\n");
 		}
+		br.close();
 	}
 }
