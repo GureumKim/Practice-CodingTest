e4f4ad68a92faf4ec57a8486eb96846d3241e28f
GureumKim <90162048+GureumKim@users.noreply.github.com>
2024-07-30 23:32:32 +0900

    [Silver V] Title: 나이순 정렬, Time: 168 ms, Memory: 37672 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -2,24 +2,33 @@ import sys
 input = sys.stdin.readline
 
 
+# def get_list():
+    # lst = []
+    # for i in range(int(input())):
+    #     age, name = input().split()
+    #     lst.append((int(age), name, i))
+    # return lst
 def get_list():
-    lst = []
+    lst = [[] for _ in range(201)]
     for i in range(int(input())):
         age, name = input().split()
-        lst.append((int(age), name, i))
+        lst[int(age)].append(name)
     return lst
 
+# def sort_member(mem):
+    # return sorted(mem, key=lambda x: (x[0], x[2]))
 
-def sort_member(mem):
-    return sorted(mem, key=lambda x: (x[0], x[2]))
 
-
+# def main():
+#     members = get_list()
+#     members = sort_member(members)
+#     for m in members:
+#         print(m[0], m[1])
 def main():
     members = get_list()
-    members = sort_member(members)
-    for m in members:
-        print(m[0], m[1])
-
-
+    for i in range(201):
+        if members[i]:
+            for m in members[i]:
+                print(i, m)
 if __name__ == "__main__":
     main()
