b6bb936983fc6995e786787233805079946ffd95
GureumKim <90162048+GureumKim@users.noreply.github.com>
2024-08-01 01:54:44 +0900

    [level 2] Title: 타겟 넘버, Time: 474.56 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,13 +1,39 @@
-def solution(numbers, target):
-    from functools import lru_cache
+answer = 0
 
-    # least recent used cache
-    # @lru_cache(maxsize)
-    @lru_cache(None)
-    def dfs(lev=0, value=0):
-        if lev == len(numbers):
-            return 1 if value == target else 0
+def solution(numbers, target, level = 0, res = 0):
+    global answer
+    
+    if level == len(numbers):
+        if res == target:
+            answer += 1 
+        return
+    
+    for k in [-1, 1]:
+        solution(numbers, target, level + 1, res + k * numbers[level])
+    
+    
+    return answer
 
-        return dfs(lev + 1, value + numbers[lev]) + dfs(lev + 1, value - numbers[lev])
+    
+    
 
-    return dfs()
+
+
+
+
+
+
+
+# def solution(numbers, target):
+#     from functools import lru_cache
+
+#     # least recent used cache
+#     # @lru_cache(maxsize)
+#     @lru_cache(None)
+#     def dfs(lev=0, value=0):
+#         if lev == len(numbers):
+#             return 1 if value == target else 0
+
+#         return dfs(lev + 1, value + numbers[lev]) + dfs(lev + 1, value - numbers[lev])
+
+#     return dfs()
