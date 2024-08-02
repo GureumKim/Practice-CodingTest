731469beb8a67263d745b690b5842f7674b19c9b
GureumKim <90162048+GureumKim@users.noreply.github.com>
2024-08-02 12:35:22 +0900

    [level 3] Title: 단어 변환, Time: 0.39 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/README.md
+++ 프로그래머스/3/43163. 단어 변환/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단어 변환 - 43163 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.25 ms
+메모리: 10 MB, 시간: 0.39 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 02일 12:01:30
+2024년 08월 02일 12:35:20
 
 ### 문제 설명
 
--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -3,22 +3,21 @@
 def solution(begin, target, words):
     from collections import deque
     
-    def compare(w1, w2):
+    def can_transform(w1, w2):
         if w1 == w2: 
             return -1
         
-        cnt, idx = 0, 0
+        cnt =  0
         for i in range(len(w1)):
             if w1[i] != w2[i]:
                 cnt += 1
                 if cnt > 1:
-                    return -1
-                idx = i
-        return idx
+                    return False
+        return True
     
     step = 0
-    q = deque()    
-    q.append((begin, 0))
+    q = deque([(begin, 0)])    
+    # q.append((begin, 0))
     
     used = [False] * len(words)
     
@@ -29,45 +28,9 @@ def solution(begin, target, words):
         
         for i in range(len(words)):
             if used[i] : continue
-            res = compare(word, words[i])
-            if res != -1:
+            
+            if can_transform(word, words[i]):
                 used[i] = True
-                q.append((word[:res] + words[i][res] + word[res+1:], step + 1))
+                q.append((words[i], step + 1))
     return 0
-
-
-
-
-
-
-
-
-
-
-# def solution(begin, target, words):
-#     from collections import deque
-
-#     wl = len(begin)
-#     words_count = len(words)
-#     q = deque()
-#     q.append((begin, 0)) # recent_word, start_idx, step
-#     used = [False]*words_count
-    
-#     while q:
-#         recent_word, step = q.popleft()
-#         if recent_word == target:
-#             return step
-        
-#         for i in range(words_count):
-#             if used[i]: continue
-#             cnt = 0
-#             for j in range(wl):
-#                 if words[i][j] != recent_word[j]:
-#                     cnt += 1
-#                 if cnt > 1: 
-#                     break
-#             if cnt == 1:
-#                 used[i] = True
-#                 q.append((words[i], step+1))
-#         # 나머지 경우는 자기 자신을 계속 추가해줄 필요가 없지                            
-#     return 0
+                
