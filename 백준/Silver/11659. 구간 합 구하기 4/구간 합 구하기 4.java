b1a5d1a8c3e23855c8e33891c496654e6eed319b
GureumKim <90162048+GureumKim@users.noreply.github.com>
2024-08-27 22:57:33 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1120 ms, Memory: 60948 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,17 +1,20 @@
-import java.util.Scanner;
-
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		//	N, M, 대상 배열 입력 받기 
-		int N = sc.nextInt();
-		int M = sc.nextInt();
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 		
 		int[] num = new int[N + 1];
+		st = new StringTokenizer(br.readLine());
 		for (int i = 1; i <= N; i++) {
-			num[i] = sc.nextInt();
+			num[i] = Integer.parseInt(st.nextToken());
 		}
 		
 		// 구간 합 배열 생성
@@ -23,9 +26,10 @@ public class Main {
 		// 구간 합 구하기
 		
 		for (int i = 0; i < M; i++) {
-			int start = sc.nextInt();
-			int end = sc.nextInt();
-			System.out.println(accSum[end] - accSum[start-1]);
+			st = new StringTokenizer(br.readLine());
+			int fromIdx = Integer.parseInt(st.nextToken());
+			int toIdx = Integer.parseInt(st.nextToken());
+			System.out.println(accSum[toIdx] - accSum[fromIdx - 1]);
 		}
 	}
 }
