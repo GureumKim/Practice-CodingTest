9da5a0286b617668420d358bcf055a31516ad9cd
GureumKim <90162048+GureumKim@users.noreply.github.com>
2024-09-11 17:57:29 +0900

    [Silver III] Title: N과 M (5), Time: 160 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.py
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -1,4 +1,26 @@
-from itertools import permutations as pm
-n,m = map(int,input().split())
-lst = list(input().split())
-print('\n'.join(map(' '.join,pm(sorted(lst,key=lambda x:(len(x),x)),m))))
+def main():
+    from sys import stdin
+    input = stdin.readline
+
+    def dfs(level):
+        if level == m:
+            print(*sequence)
+            return
+        for i in range(n):
+            if not visited[i]:
+                visited[i] = True
+                sequence[level] = num[i]
+                dfs(level + 1)
+                visited[i] = False
+
+    n, m = map(int, input().split())
+    sequence = [0] * m
+    visited = [False] * n
+    num = list(map(int, input().split()))
+    num.sort()
+
+    dfs(0)
+
+
+if __name__ == "__main__":
+    main()
