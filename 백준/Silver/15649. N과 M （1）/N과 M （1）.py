800609f828b9fa1dbad4a487b0917fb910c511b8
GureumKim <90162048+GureumKim@users.noreply.github.com>
2024-09-11 17:03:31 +0900

    [Silver III] Title: N과 M (1), Time: 152 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,15 +1,23 @@
-def dfs(n,m,level=0):
-    if level == m:
-        print(*choice)
-        return
-    for i in range(1,n+1):
-        if not used[i]:
-            used[i] = 1
-            choice[level] = i
-            dfs(n,m,level+1)
-            used[i] = 0
+def main():
+    from sys import stdin
+    input = stdin.readline
 
-n,m = map(int,input().split())
-choice = [0]*m
-used = [0]*(n+1)
-dfs(n,m)
+    def dfs(level):
+        if level == m:
+            print(*sequence)
+            return
+        for i in range(1, n + 1):
+            if not visited[i - 1]:
+                sequence[level] = i
+                visited[i - 1] = True
+                dfs(level + 1)
+                visited[i - 1] = False
+
+    n, m = map(int, input().split())
+    sequence = [0] * m
+    visited = [False] * n
+    dfs(0)
+
+
+if __name__ == "__main__":
+    main()
