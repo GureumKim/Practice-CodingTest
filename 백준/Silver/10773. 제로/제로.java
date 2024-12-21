b45e63fb22389c8465256c4036480054feb36c2a
GureumKim <90162048+GureumKim@users.noreply.github.com>
2024-12-22 01:17:19 +0900

    [Silver IV] Title: 제로, Time: 184 ms, Memory: 23184 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,26 +1,29 @@
 import java.io.BufferedReader;
-//import java.io.FileReader;
 import java.io.IOException;
-import java.util.Stack;
 import java.io.InputStreamReader;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-//		BufferedReader br = new BufferedReader(new FileReader("./input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int k = Integer.parseInt(br.readLine());
+		int[] stack =  new int[k];
+		int top = -1;
 		
-		Stack<Integer> stack = new Stack<>();
 		for (int i = 0; i < k; i++) {
 			int num = Integer.parseInt(br.readLine());
-			if (num == 0)
-				stack.pop();
-			else
-				stack.add(num);
+			if (num == 0) {
+				stack[top] = 0;
+				top--;
+			}
+			else {
+				top++;
+				stack[top] = num;
+			}
 		}
+		
 		int ans = 0;
-		while (stack.isEmpty() == false) {
-			ans += stack.pop();
+		for (int n : stack) {
+			ans += n;
 		}
 		System.out.println(ans);
 		br.close();
