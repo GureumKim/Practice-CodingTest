7cadb160476a8762529594830cc6e203171da793
GureumKim <90162048+GureumKim@users.noreply.github.com>
2025-02-19 20:46:56 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 9.07 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -1,57 +1,38 @@
 def solution(friends, gifts):
-    def comp(a,b):
-        if exchange_data[a][b] > exchange_data[b][a]:
-            result[a] += 1
-        elif exchange_data[a][b] < exchange_data[b][a]:
-            result[b] += 1
-        else:
-            comp_idx(a,b)
+    from collections import defaultdict
+    answer = 0
+    name = defaultdict(int)
+    for i, f in enumerate(friends):
+        name[f] = i
+    
+    give = [[0] * len(friends) for _ in range(len(friends))]
+    receive = [[0] * len(friends) for _ in range(len(friends))]
+    rate = [0] * len(friends)
+    res = [0] * len(friends)
+    
+    for gift in gifts:
+        g, r = gift.split()
+        g, r = name[g], name[r]
+        
+        give[g][r] += 1
+        receive[r][g] += 1
+        rate[g] += 1
+        rate[r] -= 1
+        
+    for i in range(len(friends)):
+        for j in range(i+1, len(friends)):
+            if give[i][j] == give[j][i]:
+                if rate[i] > rate[j]:
+                    res[i] += 1
+                elif rate[i] < rate[j]:
+                    res[j] += 1
+                continue
             
-        
-            
-    def comp_idx(a,b):
-        if exchange_data[a][a] == exchange_data[b][b]:
-            return
-        elif exchange_data[a][a] > exchange_data[b][b]:
-            result[a] += 1
-        else:
-            result[b] += 1
-    
-    
-    num_of_friends = len(friends)
-    
-    friends_dict = dict()
-    # friends' names are the key of the dict
-    for i in range(num_of_friends):
-        friends_dict[friends[i]] = i
-        
-    # exchange_data[idx][idx]는 선물 지수
-    # 나머지는 exchange_data[giver][receiver], receiver에게 선물 몇 개 줬는지 저장
-    exchange_data = [[0]*num_of_friends for _ in range(num_of_friends)]
-       
-    for g in gifts:
-        giver, receiver = g.split()
-        
-        idx_g, idx_r = friends_dict[giver], friends_dict[receiver]
-        exchange_data[idx_g][idx_r] += 1
-        
-        exchange_data[idx_g][idx_g] += 1
-        exchange_data[idx_r][idx_r] -= 1
-        
-        
-    result = [0]*num_of_friends
-    
-    # i+1으로 불필요한 연산 제거
-    for i in range(num_of_friends):
-        for j in range(i+1,num_of_friends):
-            # n : 0 번 교환일 수도 있다!! (반례)
-            if exchange_data[i][j] or exchange_data[j][i]:
-                comp(i,j)
+            if give[i][j] > give[j][i]:
+                res[i] += 1
             else:
-                comp_idx(i,j)
-            
+                res[j] += 1
     
-    answer = max(result)
-        
+    answer = max(res)
     
     return answer
