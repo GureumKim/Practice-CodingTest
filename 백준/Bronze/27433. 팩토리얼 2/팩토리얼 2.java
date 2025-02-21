6dac972a3e9d0b33e0cd0d1b15377972983b5d4d
GureumKim <90162048+GureumKim@users.noreply.github.com>
2025-02-21 17:18:16 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 172 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println(factorial(n));
+	}
+	
+	public static long factorial(int n) {
+		if (n == 0)
+			return 1;
+		return (long) n * factorial(n - 1);
+	}
+}
