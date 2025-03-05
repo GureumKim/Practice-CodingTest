8cea71b4aaaed43bf980af3752eb69120309ceff
GureumKim <90162048+GureumKim@users.noreply.github.com>
2025-03-05 23:59:43 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 1184.70 ms, Memory: 172 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/README.md
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/README.md
@@ -1,6 +1,6 @@
 # [level 2] 도넛과 막대 그래프 - 258711 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258711) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258711?language=python3) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 05일 23:59:06
+2025년 03월 05일 23:59:41
 
 ### 문제 설명
 
--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.py
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.py
@@ -3,23 +3,7 @@ def solution(edges):
     edge_cnt = defaultdict(lambda: [0, 0])
     
     for s, e in edges:
-        edge_cnt[s][0] += 1 #out
-        edge_cnt[e][1] += 1 #in
+        edge_cnt[s][0] += 1
+        edge_cnt[e][1] += 1
     
-    
-    link_node = 0
-    donut = bar = eight = 0
-    for node in edge_cnt.keys():
-        if edge_cnt[node][1] == 0 and edge_cnt[node][0] >= 2:
-            link_node = node
-            
-        elif edge_cnt[node][0] == 0:
-            bar += 1
-        
-        elif edge_cnt[node][0] == 2 and edge_cnt[node][1] >= 2: # link 노드로부터 시작되는 간선 유의
-            eight += 1
-    
-    donut = edge_cnt[link_node][0] - eight - bar
-
-    
-    return [link_node, donut, bar, eight]
+    return 
