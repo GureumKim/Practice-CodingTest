f89216f72447994c470bc48afc7c1308ccd490a8
GureumKim <90162048+GureumKim@users.noreply.github.com>
2025-03-15 21:38:37 +0900

    [Gold IV] Title: 최단경로, Time: 928 ms, Memory: 74900 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,34 +1,47 @@
-import heapq
+from sys import stdin
+import heapq as hq
+
 
 def dijkstra(start):
-    visited = [0]*(V+1)
+    visited = [0] * (V + 1)
     heap = []
-    heapq.heappush(heap,(0,start))
-    # 자기 자신도 경유할 거기 때문에 미리 visited에 체크하지 않음!
-    # visited[start]=1
+    hq.heappush(heap, (0, start))  # 자기 자신(start)도 경유하기 때문에 visited 체크 x
     res[start] = 0
 
     while heap:
-        v_cost, via = heapq.heappop(heap)
+        v_cost, via = hq.heappop(heap)
         if visited[via]: continue
         visited[via] = 1
 
         if res[via] < v_cost: continue
-        for next, c in adj[via]:
-            cost = c+ v_cost
-            if res[next] > cost:
-                res[next] = cost
-                heapq.heappush(heap,(res[next],next))
+        for nxt, c in adj[via]:
+            cost = c + v_cost
+            if res[nxt] > cost:
+                res[nxt] = cost
+                hq.heappush(heap, [res[nxt], nxt])
 
-V, E = map(int,input().split())
-adj = [[] for _ in range(V+1)]
-start = int(input())
+
+input = stdin.readline
+
+V, E = map(int, input().split())
+K = int(input())
+
+adj = [[] for _ in range(V + 1)]
+
 for _ in range(E):
-    u,v,w = map(int,input().split())
-    adj[u].append((v,w))
+    u, v, w = map(int, input().split())
+    adj[u].append([v, w])
 
 INF = float('inf')
-res = [INF]*(V+1)
-dijkstra(start)
-for i in range(1,V+1):
-    print("INF" if res[i]==INF else res[i])
+res = [INF] * (V + 1)
+
+dijkstra(K)
+
+answer = []
+for i in range(1, V + 1):
+    if res[i] == INF:
+        answer.append("INF")
+    else:
+        answer.append(f"{res[i]}")
+
+print(*answer, sep="\n")
