51f6c7ddd16d28415cdac93e321e7d41d5092251
GureumKim <90162048+GureumKim@users.noreply.github.com>
2025-10-25 13:29:27 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.04 ms, Memory: 9.39 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -17,12 +17,14 @@ def solution(S):
     for s in S:
         if s in table.values():
             if word:
-                answer = table.get(word)
+                # 여기 처음에는 answer = table.get(word) 였는데 맞았다고 뜸...
+                answer += table.get(word)
                 word = ''
             answer += s
         else:
             word += s
     
+        # 키 확인은 word in table 이렇게 하면 해시 조회해서 다 빠르다고 함..
         if word in table.keys():
             answer += table.get(word)
             word = ''
